969666be07474be3d7fb0224e652e4bf3d097d2c
Mateusz Ossoliński <mateusz.ossolinski@gmail.com>
2023-11-13 22:29:38 +0100

    initial usage of shadcn with new nextjs config

---
+++ components/Navigation/index.tsx
@@ -0,0 +1,32 @@
+'use client'
+
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu"
+import Link from "next/link"
+
+const menuItems = [
+  { id: 1, label: "Home", href: "/" },
+  { id: 2, label: "Cars", href: "/cars" },
+]
+
+export const Navigation = () => (
+<NavigationMenu>
+  <NavigationMenuList>
+    <NavigationMenuItem>
+    <Link href="/docs" legacyBehavior passHref>
+      <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+        Documentation
+      </NavigationMenuLink>
+    </Link>
+    </NavigationMenuItem>
+  </NavigationMenuList>
+</NavigationMenu>
+
+)
