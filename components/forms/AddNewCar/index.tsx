4935539c3d435e08e7136fb3a7fcc49d4d8e2c5d
Mateusz Ossoliński <mateusz.ossolinski@gmail.com>
2023-11-25 01:27:26 +0100

    fixed add new car form

--- components/forms/AddNewCar/index.tsx
+++ components/forms/AddNewCar/index.tsx
@@ -2,7 +2,7 @@
 
 import { createCar } from "@/app/api/car/route";
 import { Brand } from "@/app/settings/page";
-import { useFormStatus, useFormState } from "react-dom";
+import { useFormState } from "react-dom";
 
 const formConfig = [
   {
@@ -27,15 +27,8 @@ const formConfig = [
   },
 ];
 
-export const AddNewCar = async ({ brands }: { brands: Brand[] }) => {
+export const AddNewCar = ({ brands }: { brands: Brand[] }) => {
   const [state2, formAction2] = useFormState(createCar, null);
-
-  /** TODO:
-   * create ui component for select
-   * add labels for select
-   * pass id as value and then find this id in db in brands table
-   * validate form before it's sent
-   */
 
   return (
     <div>
@@ -67,7 +60,7 @@ export const AddNewCar = async ({ brands }: { brands: Brand[] }) => {
         <select
           id="isHighlighted"
           name="isHighlighted"
-          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          className="mt-5 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
         >
           <option value="true">True</option>
           <option value="false">False</option>
