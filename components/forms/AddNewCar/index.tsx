8aac4a7d5753434fdaea8ed42c534f367fd57ead
Mateusz Ossoliński <mateusz.ossolinski@gmail.com>
2023-11-25 22:49:22 +0100

    added zod validation

--- components/forms/AddNewCar/index.tsx
+++ components/forms/AddNewCar/index.tsx
@@ -1,42 +1,68 @@
 "use client";
+import { z } from "zod";
 
-import { createCar } from "@/app/api/car/route";
 import { Brand } from "@/app/settings/page";
-import { useFormState } from "react-dom";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 
-const formConfig = [
-  {
-    id: "model",
-    label: "Model",
-    type: "text",
-  },
-  {
-    id: "generation",
-    label: "Generation",
-    type: "text",
-  },
-  {
-    id: "engine",
-    label: "Engine",
-    type: "text",
-  },
-  {
-    id: "price",
-    label: "Price",
-    type: "number",
-  },
-];
+const createCarSchema = z.object({
+  brand: z.string().min(1, "Brand is required"),
+  model: z.string().min(1, "Model name is required"),
+  generation: z.string().min(1, "Generation name is required"),
+  engine: z.string().min(1, "Engine name is required"),
+  price: z.coerce.number().min(1, "Price name is required"),
+  isHighlighted: z.boolean().default(false),
+});
+
+type CreateCarTypeSchema = z.infer<typeof createCarSchema>;
+
+export async function createCar(formData: CreateCarTypeSchema) {
+  const result = createCarSchema.safeParse(formData);
+
+  if (!result.success) {
+    throw new Error(
+      `An error occurred while parsing the create car form data.`,
+    );
+  }
+
+  try {
+    await fetch("http://localhost:5000/api/cars", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ ...formData }),
+    });
+  } catch (e) {
+    return { message: "Failed to create todo" };
+  }
+}
 
 export const AddNewCar = ({ brands }: { brands: Brand[] }) => {
-  const [state2, formAction2] = useFormState(createCar, null);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<CreateCarTypeSchema>({
+    resolver: zodResolver(createCarSchema),
+    defaultValues: {
+      brand: "",
+      model: "",
+      generation: "",
+      engine: "",
+      price: 0,
+      isHighlighted: false,
+    },
+  });
 
   return (
     <div>
-      <form action={formAction2} className="flex flex-col">
+      <form onSubmit={handleSubmit(createCar)} className="flex flex-col">
+        <label htmlFor="brand">Brand</label>
         <select
           id="brand"
-          name="brand"
           className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          {...register("brand")}
         >
           {brands.map(({ id, name }) => (
             <option key={id} value={name}>
@@ -44,27 +70,65 @@ export const AddNewCar = ({ brands }: { brands: Brand[] }) => {
             </option>
           ))}
         </select>
-        {formConfig.map(({ id, label, type }) => (
-          <span key={id}>
-            <label htmlFor={id} key={id}>
-              {label}
-            </label>
-            <input
-              id={id}
-              name={id}
-              type={type}
-              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            />
-          </span>
-        ))}
+        {errors?.brand?.message && (
+          <p className="text-sm text-red-400">{errors?.brand?.message}</p>
+        )}
+
+        <label htmlFor="model">Model</label>
+        <input
+          id="model"
+          type="text"
+          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          {...register("model")}
+        />
+        {errors?.model?.message && (
+          <p className="text-sm text-red-400">{errors?.model?.message}</p>
+        )}
+
+        <label htmlFor="generation">Generation</label>
+        <input
+          id="generation"
+          type="text"
+          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          {...register("generation")}
+        />
+        {errors?.generation?.message && (
+          <p className="text-sm text-red-400">{errors?.generation?.message}</p>
+        )}
+
+        <label htmlFor="engine">Engine</label>
+        <input
+          id="engine"
+          type="text"
+          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          {...register("engine")}
+        />
+        {errors?.engine?.message && (
+          <p className="text-sm text-red-400">{errors?.engine?.message}</p>
+        )}
+
+        <label htmlFor="price">Price</label>
+        <input
+          id="price"
+          type="number"
+          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          {...register("price", { valueAsNumber: true })}
+        />
+        {errors?.price?.message && (
+          <p className="text-sm text-red-400">{errors?.price?.message}</p>
+        )}
+
         <select
           id="isHighlighted"
-          name="isHighlighted"
           className="mt-5 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          {...register("isHighlighted", {
+            required: false,
+          })}
         >
           <option value="true">True</option>
           <option value="false">False</option>
         </select>
+
         <button
           type="submit"
           className="flex justify-center mt-5 focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
