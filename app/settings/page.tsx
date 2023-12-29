d2d3acfe598bfb6066911d1badaa67e08912028f
Mateusz Ossoliński <mateusz.ossolinski@gmail.com>
2023-12-29 22:03:10 +0100

    added simple redirection while page is accessed without jwt token stored

--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -1,8 +1,23 @@
+"use client";
+
 import { BrandsTable } from "@/components/BrandsTable";
 import { AddNewBrand } from "@/components/forms/AddNewBrand";
 import { AddNewCar } from "@/components/forms/AddNewCar";
+import { useAuthProvider } from "../AuthProvider";
+import { useRouter } from "next/navigation";
 
-export default async function Settings() {
+export default function Settings() {
+  const { push } = useRouter();
+  const {
+    authState: { jwtToken },
+  } = useAuthProvider();
+
+  if (!jwtToken) {
+    push("/");
+
+    return null;
+  }
+
   return (
     <main className="flex justify-start min-h-screen flex-col items-center  p-24">
       <AddNewCar />
