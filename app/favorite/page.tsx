dc5ca502a5fb08a4364e9cef6c2630b87b0e2b07
Mateusz Ossoliński <mateusz.ossolinski@gmail.com>
2024-01-05 18:16:36 +0100

    added check for jwt token in favorites page to prohibit access for unasigned user

--- app/favorite/page.tsx
+++ app/favorite/page.tsx
@@ -1,4 +1,16 @@
+"use client";
+
+import { useAuthProvider } from "../AuthProvider";
+
 export default function Favorite() {
+  const {
+    authState: { jwtToken },
+  } = useAuthProvider();
+
+  if (!jwtToken) {
+    return <div>Unauthorized</div>;
+  }
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       favorite
