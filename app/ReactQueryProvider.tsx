cd413e8f107fcbb651beb4a216657992ea52a829
Mateusz Ossoliński <mateusz.ossolinski@gmail.com>
2024-01-13 18:15:25 +0100

    removed hydration boundary

--- app/ReactQueryProvider.tsx
+++ app/ReactQueryProvider.tsx
@@ -1,21 +1,12 @@
 "use client";
 
-import {
-  HydrationBoundary,
-  QueryClient,
-  QueryClientProvider,
-  dehydrate,
-} from "@tanstack/react-query";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactNode, useState } from "react";
 
 export const ReactQueryProvider = ({ children }: { children: ReactNode }) => {
   const [queryClient] = useState(() => new QueryClient());
 
-  const dehydratedState = dehydrate(queryClient);
-
   return (
-    <QueryClientProvider client={queryClient}>
-      <HydrationBoundary state={dehydratedState}>{children}</HydrationBoundary>
-    </QueryClientProvider>
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
   );
 };
